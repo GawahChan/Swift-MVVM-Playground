5729d4d31485a781fe38d4f8d09135bd738b56cd
Gawah Chan <gawahchan@gmail.com>
2023-08-16 15:50:16 +0100

    Initial Commit

---
+++ Swift-MVVM-Playground/Swift_MVVM_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_MVVM_PlaygroundApp.swift
+//  Swift-MVVM-Playground
+//
+//  Created by Gawah Chan on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_MVVM_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
